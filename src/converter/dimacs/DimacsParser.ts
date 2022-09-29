c9ebae3a06ee9f607f806623e69725a2296fc866
nickp <nickposer2102@gmail.com>
2022-09-29 17:31:47 +0200

    Added converter between dimacs and logical expression format

---
+++ src/converter/dimacs/DimacsParser.ts
@@ -0,0 +1,158 @@
+import { Lexer, Token } from "@jlguenego/lexer";
+import { andRule, commentRule, negateRule, orRule, problem, problemTypeKeywords, regexComment, sat, startKeywords } from "./Syntax/DimacsSyntax";
+import { and, or, negate, open, close, variable, parenthesesRule, variableRule, blankRule, regexBlank, variableAssignment } from "./Syntax/CommonSyntax";
+
+// the order is important - tokens are applied from first to last
+const rules = [commentRule, ...startKeywords, ...problemTypeKeywords, ...parenthesesRule, andRule, orRule, negateRule, variableRule, blankRule];
+
+//Inspired by https://github.com/Robbepop/dimacs-parser/blob/master/src/items.rs
+export class DimacsParser {
+    private lex: Lexer = new Lexer(rules);
+    private tokenIterator!: IterableIterator<Token>;
+    private output: string = '';
+    private variables: Set<number> = new Set<number>();
+
+    public parse(text: string): string {
+        this.output = '';
+
+        let tokens: Token[];
+        try {
+            tokens = this.lex.tokenize(text);
+            this.tokenIterator = tokens[Symbol.iterator]()
+        } catch (e: any) {
+            throw new Error(
+                "Couldn't tokenize the input string:\n"
+                + e.message)
+        }
+        if (tokens.length == 0) return this.output;
+
+        //Problem token
+        let token = this.nextToken();
+        if (token.name != problem) new Error("Must start with problem description");
+
+        //Problem type token
+        token = this.nextToken();
+        switch (token.name) {
+            case sat:
+                this.parseSAT()
+                break;
+            default:
+                new Error("Problem " + token.name + " is not supported");
+        }
+
+        //Replace with variable names from comments if possible
+        let aliases = this.getVariableAliases(text);
+        aliases.forEach((alias, num, _) => {
+            //Workaround split, join to replace all
+            this.output = this.output.split(num.toString()).join(alias);
+        });
+
+        return this.output;
+    }
+
+    private getVariableAliases(text: string): Map<number, string> {
+        let aliases = new Map<number, string>();
+
+        let match = text.match(regexComment);
+        if (match == null) return aliases;
+
+        for (let m of match) {
+            let trimmed = m.replace(regexBlank, '');
+
+            //Remove c at beginning
+            trimmed = trimmed.substring(1);
+
+            //Comments must be of type "Number variableAssignment AliasStr"
+            let vars = trimmed.split(variableAssignment);
+            if (vars.length != 2) continue;
+            if (!/\d/.test(vars[0])) continue;
+
+            aliases.set(+vars[0], vars[1]);
+        }
+
+        return aliases;
+    }
+
+    private parseSAT() {
+        //Determine variable count
+        let token = this.nextToken()
+        let expectedVariables = +token.lexeme;
+
+        //Loop through body
+        let current = this.tokenIterator.next();
+        while (!current.done) {
+            this.parseFormula(current.value as Token);
+            current = this.tokenIterator.next();
+        }
+
+        //Check variable count
+        let actualVariables = this.variables.size;
+        if (expectedVariables != actualVariables) {
+            throw new Error(`Expected ${expectedVariables} variables, but received ${actualVariables}`);
+        }
+    }
+
+    private parseFormula(token: Token) {
+        switch (token.name) {
+            case variable:
+                this.variables.add(+token.lexeme);
+                this.output += token.lexeme;
+                break;
+            case negate:
+                this.output += `${negate} `;
+                break;
+            case open:
+                token = this.nextToken();
+                while (token.name != close) {
+                    this.parseFormula(token);
+                    token = this.nextToken();
+                }
+                break;
+            case and:
+                this.parseFormulaParams(and);
+                break;
+            case or:
+                this.parseFormulaParams(or);
+                break;
+            default:
+                throw new Error(`Unexpected token ${token.name} in formula at line ${token.position.line} column ${token.position.col}`);
+        }
+    }
+
+    private parseFormulaParams(operator: string) {
+        this.expectNextToken(open);
+        this.output += '(';
+
+        let token = this.nextToken();
+        let lastTokenType = '';
+
+        // Parse all formulas inside the parentheses
+        // Properly format them with log exp operators
+        while (token.name != close) {
+            this.parseFormula(token);
+
+            lastTokenType = token.name;
+            token = this.nextToken();
+            if (token.name != close && lastTokenType != negate) {
+                this.output += ` ${operator} `;
+            }
+        }
+
+        this.output += ')';
+    }
+
+    private nextToken(): Token {
+        let current = this.tokenIterator.next();
+        if (current.done) throw new Error('Tokens ended unexpectedly');
+
+        return current.value as Token;
+    }
+
+    private expectNextToken(tokenType: string): Token {
+        let token = this.nextToken();
+        if (token.name != tokenType)
+            throw new Error(`Expected ${tokenType} but received ${token.name} (${token.lexeme}) at line ${token.position.line} column ${token.position.col}`);
+
+        return token;
+    }
+}
---
+++ src/converter/dimacs/LogExpParser.ts
@@ -0,0 +1,242 @@
+import { Lexer, Token } from "@jlguenego/lexer";
+import { and, or, open, close, negatedVariable, parenthesesRule, variableRule, blankRule, variable, variableAssignment } from "./Syntax/CommonSyntax";
+import { andRule, negatedVariableRule, negateRule, orRule } from "./Syntax/LogExpSyntax";
+
+// the order is important - tokens are applied from first to last
+const rules = [...parenthesesRule, orRule, andRule, negatedVariableRule, variableRule, blankRule];
+const rulesNegation = [negateRule, variableRule, blankRule];
+
+interface LeveledToken {
+    level: number,
+    token: Token
+}
+
+export class LogExpParser {
+    private lex: Lexer = new Lexer(rules);
+    private lexNegation: Lexer = new Lexer(rulesNegation);
+    private output: string = '';
+
+    public parse(text: string): string {
+        this.output = '';
+
+        let tokens = this.lex.tokenize(text);
+        if (tokens.length == 0) return this.output;
+
+        //Verify formula integrity
+        let errors = this.verifyTokens(tokens);
+        if (errors.length > 0) {
+            let errorStr = '';
+            for (let error of errors) errorStr += error + '\n';
+
+            throw new Error(errorStr);
+        }
+
+        //Handle variable comments and program start
+        let variables = this.convertVariables(tokens);
+        variables.forEach((num, str, _) => {
+            this.output += `c ${num} ${variableAssignment} ${str}\n`;
+        })
+        this.output += `p sat ${variables.size}\n`;
+
+        this.parseFormula(tokens);
+
+        return this.output;
+    }
+
+    private verifyTokens(tokens: Token[]): string[] {
+        let errors: string[] = [];
+        let wasOperator = false;
+        let wasVariable = false;
+        let wasClose = false;
+
+        if (tokens.length > 0) {
+            switch (tokens[0].name) {
+                case or:
+                case and:
+                    errors.push("Formula can't start with operator (AND/OR)")
+                    break;
+            }
+
+            switch (tokens[tokens.length - 1].name) {
+                case or:
+                case and:
+                    errors.push("Formula can't end with operator (AND/OR)")
+                    break;
+            }
+        }
+
+        for (let token of tokens) {
+            switch (token.name) {
+                case or:
+                case and:
+                    if (wasOperator) errors.push("Two operators (AND/OR) can't be next to each other");
+                    break;
+                case negatedVariable:
+                case variable:
+                    if (wasVariable) errors.push(`Variable '${token.lexeme}' is next to another variable`);
+                    if (wasClose) errors.push(`Variable '${token.lexeme}' is after a closing parenthesis`);
+                    break;
+                case open:
+                    if (wasVariable) errors.push("Parenthesis can't be opened after a variable")
+                    break;
+            }
+
+            wasOperator = token.name == and || token.name == or;
+            wasVariable = token.name == variable || token.name == negatedVariable;
+            wasClose = token.name == close;
+        }
+
+        return errors;
+    }
+    /**
+     * Converts any negative variable's negative prefix
+     * Converts all variables to numbers
+     * @param tokens {Token[]} array of tokens to search for variables
+     * @returns {Map<string, number>} variables mapped to their number representation
+     */
+    private convertVariables(tokens: Token[]): Map<string, number> {
+        let variables = new Map<string, number>();
+        const addToMap = (str: string): number => {
+            let number = variables.get(str);
+            if (number ==  undefined) {
+                number = variables.size + 1;
+                variables.set(str, number);
+            }
+
+            return number;
+        };
+
+        for (let i = 0; i < tokens.length; i++) {
+            switch (tokens[i].name) {
+                case variable:
+                    tokens[i].lexeme = addToMap(tokens[i].lexeme).toString();
+                    break;
+                case negatedVariable:
+                    let negTokens = this.lexNegation.tokenize(tokens[i].lexeme);
+                    if (negTokens.length != 2) throw new Error("Can't parse negated variables that don't have two tokens")
+
+                    let negToken = negTokens[1] as Token;
+                    let varNumber = addToMap(negToken.lexeme);
+
+                    //Replace negated variable with dimacs negative variable syntax
+                    negToken.lexeme = `-${varNumber}`;
+                    tokens[i] = negToken;
+                    break;
+            }
+        }
+
+        return variables;
+    }
+
+    private getLeveledTokens(tokens: Token[]): LeveledToken[] {
+        let leveledTokens: LeveledToken[] = [];
+        let curLevel = 0;
+
+        for (let token of tokens) {
+            switch (token.name) {
+                case open:
+                    curLevel++;
+                    break;
+                case close:
+                    curLevel--;
+                    break;
+                default:
+                    leveledTokens.push({token: token, level: curLevel});
+                    break;
+            }
+        }
+
+        return leveledTokens;
+    }
+
+    private parseFormula(tokens: Token[]) {
+        let leveledTokens = this.getLeveledTokens(tokens);
+
+        //Start recursive parsing of formula starting with or
+        this.processOR(leveledTokens);
+    }
+
+    private getOperatorPositions(tokens: LeveledToken[], operator: string, level: number): number[] {
+        let positions: number[] = [] ;
+        for (let i = 0; i < tokens.length; i++) {
+            let token = tokens[i];
+            switch (token.token.name) {
+                case operator:
+                    if (token.level == level) positions.push(i);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        //Always add token length
+        positions.push(tokens.length);
+
+        return positions;
+    }
+
+    private getMinLevel(tokens: LeveledToken[]): number {
+        let minLevel = undefined;
+
+        for (const token of tokens) {
+            if (minLevel == undefined || token.level < minLevel) {
+                minLevel = token.level;
+            }
+        }
+
+        return minLevel ?? -1;
+    }
+
+    private processAND(tokens: LeveledToken[]): LeveledToken[] {
+        if (tokens.length < 2) {
+            this.output += ` ${tokens[0].token.lexeme} `;
+            return tokens;
+        }
+
+        let level = this.getMinLevel(tokens);
+        let operatorPositions = this.getOperatorPositions(tokens, and, level);
+        if (operatorPositions.length == 1) return this.processOR(tokens);
+
+        let returnTokens: LeveledToken[] = [];
+        let lastOr = -1;
+
+        this.output += `*(`;
+        for (let pos of operatorPositions) {
+            let before = tokens.slice(lastOr + 1, pos);
+            lastOr = pos;
+
+            let processedBefore = this.processOR(before);
+            returnTokens.push(...processedBefore);
+        }
+        this.output += `)`;
+
+        return returnTokens;
+    }
+
+    private processOR(tokens: LeveledToken[]): LeveledToken[] {
+        if (tokens.length < 2) {
+            this.output += ` ${tokens[0].token.lexeme} `;
+            return tokens;
+        }
+
+        let level = this.getMinLevel(tokens);
+        let operatorPositions = this.getOperatorPositions(tokens, or, level);
+        if (operatorPositions.length == 1) return this.processAND(tokens);
+
+        let returnTokens: LeveledToken[] = [];
+        let lastOr = -1;
+
+        if (operatorPositions.length > 1) this.output += `+(`;
+        for (let pos of operatorPositions) {
+            let before = tokens.slice(lastOr + 1, pos);
+            lastOr = pos;
+
+            //Start processing nested expressions with OR
+            let processedBefore = before[0].level != level ? this.processOR(before) : this.processAND(before);
+            returnTokens.push(...processedBefore);
+        }
+        if (operatorPositions.length > 1) this.output += `)`;
+
+        return returnTokens;
+    }
+}
