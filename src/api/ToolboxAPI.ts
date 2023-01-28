3aa0a2f659c3916cdad2f6f1d1480877aee95cfe
nickp <nickposer2102@gmail.com>
2023-01-29 00:08:09 +0100

    Replaced await with then()

--- src/api/ToolboxAPI.ts
+++ src/api/ToolboxAPI.ts
@@ -2,44 +2,43 @@ import {Solution} from "../components/solvers/Solution";
 import {ProblemSolver} from "../components/solvers/ProblemSolver";
 
 export async function postProblem(problemType: string, content: any, solver: ProblemSolver | undefined): Promise<Solution> {
-    let returnData: Solution = { debugData: "", error: "", metaData: "", solutionData: "" };
-
-    await fetch(`${process.env.API_BASE_URL}/solve/${problemType}`, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            requestContent: content,
-            requestedSolverId: solver?.id
+    return fetch(
+        `${process.env.API_BASE_URL}/solve/${problemType}`,
+        {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                requestContent: content,
+                requestedSolverId: solver?.id
+            })
         })
-    }).then(async (res) => {
-        returnData = await res.json() as Solution;
-    }).catch(reason => {
-        returnData = {
-            solutionData: "",
-            debugData: "",
-            metaData: "",
-            error: `${reason}`,
-        }
-    });
-
-    return returnData;
+        .then(response => response.json())
+        .then(json => json as Solution)
+        .catch(reason => {
+            return {
+                solutionData: "",
+                debugData: "",
+                metaData: "",
+                error: `${reason}`,
+            }
+        });
 }
 
 export async function fetchSolvers(problemType: string): Promise<ProblemSolver[]> {
-    let result: ProblemSolver[] = [];
-
-    await fetch(`${process.env.API_BASE_URL}/solvers/${problemType}`, {
-        method: "GET",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    }).then(async (res) => {
-        result = await res.json() as ProblemSolver[];
-    }).catch(reason => {
-        console.log(reason)
-    });
-
-    return result;
+    return fetch(
+        `${process.env.API_BASE_URL}/solvers/${problemType}`,
+        {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(response => response.json())
+        .then(json => json as ProblemSolver[])
+        .catch(reason => {
+            console.log(reason)
+            return []
+        });
 }
