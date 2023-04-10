d9b0ff47ee63ebdb0132f42a11d1be47fb4ef9b3
nickp <nickposer2102@gmail.com>
2023-04-10 16:04:14 +0200

    Use TextInputMask for Feature Model Anomalies

--- src/pages/solve/FeatureModelAnomaly.tsx
+++ src/pages/solve/FeatureModelAnomaly.tsx
@@ -1,10 +1,53 @@
+import { Center, Divider, VStack } from "@chakra-ui/react";
 import { NextPage } from "next";
-import React from "react";
-import { FeatureModel } from "../../components/solvers/FeatureModel/FeatureModel";
+import React, { useState } from "react";
+import { MultiSelect } from "react-multi-select-component";
+import { ProgressHandler } from "../../components/solvers/ProgressHandler";
+import { TextInputMask } from "../../components/solvers/TextInputMask";
+
+const anomalies = [{
+    label: "Void Feature Model",
+    value: "feature-model/anomaly/void",
+}, {
+    label: "Dead Features",
+    value: "feature-model/anomaly/dead",
+}, {
+    label: "False-Optional Features",
+    value: "feature-model/anomaly/false-optional",
+}, {
+    label: "Redundant Constraints",
+    value: "feature-model/anomaly/redundant-constraints",
+}];
 
 const FeatureModelAnomaly: NextPage = () => {
+    const [uvl, setUvl] = useState<string>("");
+    const [selectedAnomalies, setSelectedAnomalies] = useState<any[]>(anomalies);
+
     return (
-        <FeatureModel type="anomaly"/>
+        <TextInputMask
+            title="Feature Model Anomaly Solver"
+            description="For a tree of features with cross tree constraints, the solver detects anomalies."
+            textPlaceholder="Enter your feature model in UVL format"
+            onTextChanged={setUvl}
+            body={
+                <VStack>
+                    <Center width="100px" >
+                        <MultiSelect
+
+                            options={anomalies}
+                            value={selectedAnomalies}
+                            onChange={setSelectedAnomalies}
+                            labelledBy="Select anomalies"/>
+                    </Center>
+
+                    <Divider/>
+
+                    <ProgressHandler
+                        explicitSolvers={selectedAnomalies.map(a => a.value)}
+                        problemUrl={`feature-model/anomaly`}
+                        problemInput={uvl}/>
+                </VStack>
+            }/>
     );
 };
 export default FeatureModelAnomaly;
