4c2a12cf62c6694fe93606c5130f41841ab4c27b
nickp <nickposer2102@gmail.com>
2023-04-21 11:07:16 +0200

    Add comment for requestedSolverId

--- src/components/solvers/SolveRequest.ts
+++ src/components/solvers/SolveRequest.ts
@@ -1,5 +1,8 @@
 export interface SolveRequest {
     requestContent?: any;
+    /**
+     * If no solver id is provided, the toolbox choose the solver itself via meta solver strategy
+     */
     requestedSolverId?: string,
     requestedSubSolveRequests: Map<string, SolveRequest>
 }
