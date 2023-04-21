680ceb061890618d2d6a55b6ea021f331abe0d74
nickp <nickposer2102@gmail.com>
2023-04-21 12:15:45 +0200

    Add description for requestedSubSolveRequests

--- src/components/solvers/SolveRequest.ts
+++ src/components/solvers/SolveRequest.ts
@@ -3,6 +3,9 @@ export interface SubSolveRequest {
      * If no solver id is provided, the toolbox choose the solver itself via meta solver strategy
      */
     requestedSolverId?: string,
+    /**
+     * Map from problem type to SubSolveRequest to allow explicitly requested solvers for a subroutine
+     */
     requestedSubSolveRequests: Map<string, SubSolveRequest>
 }
 
