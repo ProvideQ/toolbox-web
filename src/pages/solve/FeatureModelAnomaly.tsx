a52d49ce2f1d24950201baf36a73806d1268d9ba
Tobias Dudziak <utsbp@student.kit.edu>
2024-05-02 12:50:32 +0200

    add button, handler and css

--- src/pages/solve/FeatureModelAnomaly.tsx
+++ src/pages/solve/FeatureModelAnomaly.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Button,
   Divider,
   Heading,
   ListItem,
@@ -13,6 +14,7 @@ import { MultiSelect, Option } from "react-multi-select-component";
 import { Layout } from "../../components/layout/Layout";
 import { ProgressHandler } from "../../components/solvers/ProgressHandler";
 import { TextInputMask } from "../../components/solvers/TextInputMask";
+import {visualizeFeatureModel} from "../../api/ToolboxAPI";
 
 const anomalies: Option[] = [
   {
@@ -30,50 +32,81 @@ const FeatureModelAnomaly: NextPage = () => {
   const [selectedAnomalies, setSelectedAnomalies] =
     useState<Option[]>(anomalies);
 
-  return (
-    <Layout>
-      <Heading as="h1">Feature Model Anomaly Solvers</Heading>
-      <Text color="text" align="justify">
-        For a tree of features with cross tree constraints, these solvers can
-        detect various anomalies. Feature Models are given in the extended
-        Universal Variablity Language (UVL).
-      </Text>
-
-      <UnorderedList>
-        <ListItem>
-          A feature model is <b>void</b> if there is no valid selection of
-          features.
-        </ListItem>
-        <ListItem>
-          A feature is <b>dead</b> if the feature cannot be included in any
-          valid feature selection.
-        </ListItem>
-      </UnorderedList>
-
-      <TextInputMask
-        textPlaceholder="Enter your feature model in UVL format"
-        onTextChanged={setUvl}
-        body={
-          <VStack>
-            <Box width="300px">
-              <MultiSelect
-                options={anomalies}
-                value={selectedAnomalies}
-                onChange={setSelectedAnomalies}
-                labelledBy="Select anomalies"
-              />
-            </Box>
-
-            <Divider />
-
-            <ProgressHandler
-              problemTypes={selectedAnomalies.map((option) => option.value)}
-              problemInput={uvl}
-            />
-          </VStack>
+    const handleVisualizeClick = async () => {
+        try {
+            const svgContent = await visualizeFeatureModel(uvl);
+            if (!svgContent) {
+                console.error("No SVG content received.");
+                return;
+            }
+            let container = document.getElementById("svgContainer");
+            if (container != null) {
+                container.innerHTML = svgContent;
+            }
+        } catch (error) {
+            console.error("Error visualizing UVL:", error);
         }
-      />
-    </Layout>
+    };
+
+  return (
+      <Layout>
+          <Heading as="h1">Feature Model Anomaly Solvers</Heading>
+          <Text color="text" align="justify">
+              For a tree of features with cross tree constraints, these solvers can
+              detect various anomalies. Feature Models are given in the extended
+              Universal Variablity Language (UVL).
+          </Text>
+
+          <UnorderedList>
+              <ListItem>
+                  A feature model is <b>void</b> if there is no valid selection of
+                  features.
+              </ListItem>
+              <ListItem>
+                  A feature is <b>dead</b> if the feature cannot be included in any
+                  valid feature selection.
+              </ListItem>
+          </UnorderedList>
+
+          <TextInputMask
+              textPlaceholder="Enter your feature model in UVL format"
+              onTextChanged={setUvl}
+              body={
+                  <VStack>
+                      <Box width="300px">
+                          <MultiSelect
+                              options={anomalies}
+                              value={selectedAnomalies}
+                              onChange={setSelectedAnomalies}
+                              labelledBy="Select anomalies"
+                          />
+                      </Box>
+
+                      <Divider/>
+
+                      <ProgressHandler
+                          problemTypes={selectedAnomalies.map((option) => option.value)}
+                          problemInput={uvl}
+                      />
+                      <Button colorScheme="blue" onClick={handleVisualizeClick}>
+                          Visualize UVL
+                      </Button>
+                  </VStack>
+              }
+          />
+          <div id="svgContainer"></div>
+          <style jsx>{`
+              #svgContainer {
+                  //best working css so far
+                  position: relative;
+                  left: 25%;
+                  -webkit-transform: translateX(-50%);
+                  -ms-transform: translateX(-50%);
+                  transform: translateX(-50%);
+              }
+
+          `}</style>
+      </Layout>
   );
 };
 export default FeatureModelAnomaly;
