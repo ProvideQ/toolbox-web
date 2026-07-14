fe7e5e2687c9ffb361573f2d0dc1d050906a609f
Elscrux <nickposer2102@gmail.com>
2026-07-14 21:43:43 +0200

    fix: ProvideQ logo name

--- src/components/landing-page/Hero.tsx
+++ src/components/landing-page/Hero.tsx
@@ -4,7 +4,7 @@ export const Hero = (props: { title: string }) => (
   <Flex justifyContent="center" alignItems="center" height="60vh">
     <Link href="/">
       <HStack spacing={4}>
-        <Image src="/g100.svg" height={"6vw"} alt="ProvideQ Logo" />
+        <Image src="/ProvideQ.svg" height={"6vw"} alt="ProvideQ Logo" />
         <Heading fontSize="6vw" color="kitBlueDark">
           {props.title}
         </Heading>
