00b3d499a58f226782717e4aef91d310a7bc930e
Elscrux <nickposer2102@gmail.com>
2026-07-14 21:34:11 +0200

    chore: formatting

--- src/components/landing-page/Hero.tsx
+++ src/components/landing-page/Hero.tsx
@@ -1,18 +1,14 @@
-import { Flex, Heading, Link, Image, HStack } from "@chakra-ui/react";
+import { Flex, Heading, HStack, Image, Link } from "@chakra-ui/react";
 
 export const Hero = (props: { title: string }) => (
-  <Flex
-    justifyContent="center"
-    alignItems="center"
-    height="60vh"
-  >
+  <Flex justifyContent="center" alignItems="center" height="60vh">
     <Link href="/">
-    <HStack spacing={4}>
-      <Image src="/g100.svg" height={"6vw"} alt="ProvideQ Logo" />
-      <Heading fontSize="6vw" color="kitBlueDark">
-        {props.title}
-      </Heading>
-    </HStack>
+      <HStack spacing={4}>
+        <Image src="/g100.svg" height={"6vw"} alt="ProvideQ Logo" />
+        <Heading fontSize="6vw" color="kitBlueDark">
+          {props.title}
+        </Heading>
+      </HStack>
     </Link>
   </Flex>
 );
